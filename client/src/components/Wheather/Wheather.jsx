bf17582e7347767a539222ee8147e4084110bd99
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2025-04-25 22:52:18 +0300

    Ready basic hardcoded data for the wheather details

--- client/src/components/Wheather/Wheather.jsx
+++ client/src/components/Wheather/Wheather.jsx
@@ -6,7 +6,26 @@ export default function Wheather() {
             <div className={styles['wheather']}>
                 <div className={styles['search-bar']}>
                     <input type="text" placeholder='Search'/>
-                    <img src="" alt="" />
+                    <img src="/Search-icon.png" alt=""  className={styles['search-icon']}/>
+                </div>
+                <img src="/clear.png" alt="something" className={styles['weather-icon']} />
+                <p className={styles['temperature']}>16°c</p>
+                <p className={styles['location']}>London</p>
+                <div className={styles['wheather-data']}>
+                    <div className={styles['col']}>
+                        <img src="/humidity.png" alt="" className={styles['col-png']} />
+                        <div>
+                            <p>91 %</p>
+                            <span>Humidity</span>
+                        </div>
+                    </div>
+                    <div className={styles['col']}>
+                        <img src="/wind.png" alt="" className={styles['col-png']} />
+                        <div>
+                            <p>3.6 Km/h</p>
+                            <span>Wind Speed</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
